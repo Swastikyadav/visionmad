9d4eca15ac798b1585996dd1498f4d6680996394
Swastikyadav <swastikyadav.dev@gmail.com>
2021-01-21 13:24:11 +0530

    added core-benefits section

--- src/pages/ComingSoonPage.jsx
+++ src/pages/ComingSoonPage.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import CoreBenefits from "./coreBenefits/CoreBenefits";
 
 import "../assets/stylesheets/comingsoon.css";
 
 function ComingSoonPage() {
   return (
-    <div className="hero-section">
-      <aside className="hero-text-aside">
-        <div>
-          <h1>VisionMad</h1>
-          <p>Vision to <strong>M</strong>ake <strong>A</strong> <strong>D</strong>ifference</p>
-          <p>
-            Learn web-development through weekly news letter content.
-            Subscribe to be the first to know when we launch.
-          </p>
-          <input type="email" name="email" placeholder="Email to get notified" />
-          <button className="sub-button">Notify Me</button>
-        </div>
-      </aside>
-      <aside className="hero-media-aside">
-        <iframe src="https://player.vimeo.com/video/502500087?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="New Recording - 20/01/2021, 13:07:21"></iframe>
-      </aside>
-    </div>
+    <>
+      <div className="hero-section">
+        <aside className="hero-text-aside">
+          <div>
+            <h1><span className="underline">Vision</span>Mad</h1>
+            <p>Vision to <strong className="underline">M</strong>ake <strong className="underline">A</strong> <strong className="underline">D</strong>ifference</p>
+            <p>
+              Learn web-development through weekly news letter content.
+              Subscribe to be the first to know when we launch.
+            </p>
+            <input type="email" name="email" placeholder="Email to get notified" />
+            <button className="sub-button">Notify Me</button>
+          </div>
+        </aside>
+        <aside className="hero-media-aside">
+          <iframe src="https://player.vimeo.com/video/502500087?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="New Recording - 20/01/2021, 13:07:21"></iframe>
+        </aside>
+      </div>
+      <div className="core-benefits">
+        <CoreBenefits />
+      </div>
+    </>
   )
 }
 
---
+++ src/pages/coreBenefits/CoreBenefits.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import OneStopGuide from "../../assets/media/onestopguide.svg";
+import SkillsThatMatter from "../../assets/media/skillsthatmatter.svg";
+import ProductMindset from "../../assets/media/productmindset.svg";
+import PaymentCard from "../../assets/media/paymentcard.svg";
+import "../../assets/stylesheets/corebenefits.css";
+
+function CoreBenefits() {
+  return (
+    <>
+      <h2 className="secondary-heading"><span className="underline">Core</span> Benefits</h2>
+      <section className="benefit-section bs-1">
+        <aside className="text-info">
+          <h2><span className="underline">Your one-stop guide</span> to web-development.</h2>
+          <p>From "Hello World" to building real-world web-apps. Get out of the tutorial hell.</p>
+        </aside>
+        <aside className="illustrations">
+          <img className="benefit-img" src={OneStopGuide} alt="one-stop-guide-for-web-development"/>
+        </aside>
+      </section>
+      <section className="benefit-section bs-2">
+        <aside className="illustrations">
+          <img className="benefit-img" src={SkillsThatMatter} alt="one-stop-guide-for-web-development"/>
+        </aside>
+        <aside className="text-info">
+          <h2>Learn <span className="underline">skills that matter</span> in today's modern world.</h2>
+          <p>Forget about outdated courses, languages, and frameworks. Learn everything you need and nothing you don't.</p>
+        </aside>
+      </section>
+      <section className="benefit-section bs-3">
+        <aside className="text-info">
+          <h2>Code with <span className="underline">product mindset.</span></h2>
+          <p>Build products, not just another dummy project. At the end of every series, we create something useful for the world.</p>
+        </aside>
+        <aside className="illustrations">
+          <img className="benefit-img" src={ProductMindset} alt="one-stop-guide-for-web-development"/>
+        </aside>
+      </section>
+      <section className="benefit-section bs-4">
+        <aside className="illustrations">
+          <img className="benefit-img" src={PaymentCard} alt="one-stop-guide-for-web-development"/>
+        </aside>
+        <aside className="text-info">
+          <h2><span className="underline">No one should pay</span> to learn web-development.</h2>
+          <p>If utilized properly this platform can replace most of the paid courses out there.</p>
+        </aside>
+      </section>
+    </>
+  );
+}
+
+export default CoreBenefits;
