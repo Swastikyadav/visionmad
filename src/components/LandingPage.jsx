0677097c4f64cdf4db731647ea1b593bbc80eb3e
Swastikyadav <swastikyadav.dev@gmail.com>
2021-03-10 18:34:04 +0530

    dummy video on home page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -20,7 +20,7 @@ function LandingPage() {
           </div>
         </aside>
         <aside className="hero-media-aside">
-          <iframe id="vp1o3QQS" title="Video Player" width="640" height="360" frameborder="0" src="https://s3.amazonaws.com/embed.animoto.com/play.html?w=swf/production/vp1&e=1615282908&f=o3QQStpkY2N090WJ817eUQ&d=0&m=p&r=360p+480p+720p&volume=100&start_res=720p&i=m&asset_domain=s3-p.animoto.com&animoto_domain=animoto.com&options=" allowfullscreen></iframe>
+          <iframe id="vp17Shn8" title="Video Player" width="640" height="360" frameborder="0" src="https://s3.amazonaws.com/embed.animoto.com/play.html?w=swf/production/vp1&e=1615381356&f=7Shn8MoBi1XVvS0Pe6ShLw&d=0&m=p&r=360p+480p+720p&volume=100&start_res=720p&i=m&asset_domain=s3-p.animoto.com&animoto_domain=animoto.com&options=" allowfullscreen></iframe>
         </aside>
       </div>
       <div className="core-benefits">
