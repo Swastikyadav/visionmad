1337b8027e8a0008ed67399356d1e03191e89ab3
Swastikyadav <swastikyadav.dev@gmail.com>
2021-03-15 21:17:24 +0530

    added social icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Link } from "gatsby";
+
 import NewsLetterForm from "./NewsLetterForm";
+import { TwitterOutlined, InstagramOutlined, FacebookOutlined, YoutubeOutlined } from "@ant-design/icons";
 
 import "../assets/stylesheets/footer.css";
 
@@ -12,6 +15,11 @@ function Footer() {
         Subscribe and learn to code with product mindset.
       </p>
       <NewsLetterForm />
+
+      <Link to="https://twitter.com/visionmadHQ" className="tw-icon" target="_blank"><TwitterOutlined className="social-icon" /></Link>
+      <Link to="https://www.instagram.com/visionmadhq/" className="insta-icon" target="_blank"><InstagramOutlined className="social-icon" /></Link>
+      <Link to="https://www.facebook.com/VisionMadHQ" className="fb-icon" target="_blank"><FacebookOutlined className="social-icon" /></Link>
+      <Link to="https://www.youtube.com/channel/UCJSWJgTRVdftxZpFXWtMZgw" className="yt-icon" target="_blank"><YoutubeOutlined className="social-icon" /></Link>
     </footer>
   );
 }
--- src/pages/newsletter.jsx
+++ src/pages/newsletter.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Layout from "../components/layout";
 import NewsLetterForm from "../components/NewsLetterForm";
+import SEO from "../components/SEO";
 
 import "../assets/stylesheets/newsletterPage.css";
 
 function NewsLetter() {
   return (
     <Layout>
+      <SEO
+        title="VisionMad | Productize your web-dev knowledge."
+        description="Weekly newsletter on how to build your micro business as a web developer."
+      />
       <section class="newsletter-page">
         <h1>Productize your web-dev knowledge.</h1>
-        <p>Weekly newsletter on how to build your microbusiness as a web developer. Learn to code with a product mindset.</p>
+        <p>Weekly newsletter on how to build your micro business as a web developer. Learn to code with a product mindset.</p>
         <NewsLetterForm />
       </section>
     </Layout>
